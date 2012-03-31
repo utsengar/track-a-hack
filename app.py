6e9ab9ab4a38eae363d6775a2fc3aa012fcd5073
Utkarsh Sengar <utkarsh2012@gmail.com>
2012-03-31 16:44:36 -0700

    added more checks

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from git_actions import get_data
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    json = get_data()
     return 'Hello World!'
 
 if __name__ == '__main__':
--- git_actions.py
+++ git_actions.py
@@ -2,26 +2,30 @@ import os
 from lib.gitstats import GitDataCollector
 
 """
-
 >>> pull_project("utkarsh2012", "python-ebay")
-
 """
+
+def get_data(user_name, project):
+    project_dir_path = pull_project(user_name, project)
+
+    if os.path.exists(project_dir_path):
+        data = call_gitstats(project_dir_path)
+        _rm_rf(project_dir_path)
+        return data
+    else:
+        return "Git repo does not exist!"
+
 
 def pull_project(user_name, project):
     os.system('git clone https://github.com/' + user_name + '/' + project + '.git')
     project_dir = os.getcwd() + '/' + project
-    print project_dir
+    return project_dir
 
-    if os.path.exists(project_dir):
-        call_gitstats(project_dir)
-    else:
-        return "Git repo does not exist!"
 
 
 def call_gitstats(project_folder):
     data_collector = GitDataCollector()
     data_collector.collect(project_folder)
-    _rm_rf(project_folder)
     return data_collector
 
 
@@ -34,5 +38,9 @@ def _rm_rf(d):
     os.rmdir(d)
 
 
+def construct_json():
+    pass
+
+
 if __name__ == "__main__":
-   pull_project("utkarsh2012", "python-ebay")
+    get_data("utkarsh2012", "python-ebay")
