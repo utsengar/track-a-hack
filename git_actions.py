d4772a4c5a8bd6ae500cf8f7f33efde5749f246b
Roopesh Vaddepally <roopeshvaddepally@gmail.com>
2012-03-31 19:05:55 -0700

    return json. inefficient and redundant code here, TODO: Fix it

--- git_actions.py
+++ git_actions.py
@@ -1,9 +1,19 @@
 import os
+import datetime
+import json
+from pprint import pprint
 from lib.gitstats import GitDataCollector
 
-"""
->>> pull_project("utkarsh2012", "python-ebay")
-"""
+
+class GitStatsEncoder(json.JSONEncoder):
+    """Encoder for dealing with some attributes in git data collector object."""
+    def default(self, obj):
+        if isinstance(obj, datetime.timedelta):
+            return str(obj) # just make something up
+        if isinstance(obj, set):
+            return sorted(list(obj)) # sorted list, instead of set
+        return json.JSONEncoder.default(self, obj)
+
 
 def get_data(user_name, project):
     project_dir_path = pull_project(user_name, project)
@@ -24,8 +34,11 @@ def pull_project(user_name, project):
 
 
 def call_gitstats(project_folder):
+    curdir = os.path.abspath(os.curdir)
+    os.chdir(project_folder)
     data_collector = GitDataCollector()
     data_collector.collect(project_folder)
+    os.chdir(curdir)
     return data_collector
 
 
@@ -37,10 +50,29 @@ def _rm_rf(d):
             os.unlink(path)
     os.rmdir(d)
 
+def we_want_this_attribute(obj, attr_name):
+    """Decides if we want this attribute in the returned json.
 
-def construct_json():
-    pass
+    We don't want it, if it starts with _, get or is a method or any callable of the obj.
+    Implementation is ad hoc, and so is the name.
+    """
+    # print attr_name, attr_name.startswith("_"), attr_name.startswith("get"), callable(attr_name)
+    if attr_name.startswith("_") or attr_name.startswith("get") or callable(getattr(obj, attr_name)):
+        return False
+    else:
+        return True
 
+def construct_json(data_collector):
+    attributes = [each for each in dir(data_collector) if we_want_this_attribute(data_collector, each)]
+    d = {}
+    for each in attributes:
+        try:
+            y = getattr(data_collector, each)
+            d.update({each: y})
+        except:
+            pass
+    return json.loads(json.dumps(d, cls=GitStatsEncoder, sort_keys=True, indent=4))
 
 if __name__ == "__main__":
-    get_data("utkarsh2012", "python-ebay")
+    data = get_data("utkarsh2012", "python-ebay")
+    pprint(construct_json(data))
